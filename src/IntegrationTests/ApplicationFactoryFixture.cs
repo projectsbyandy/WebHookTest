243a0a5714af9ecf5d0a339df4604f2b2009a9b0
Andrew Chang <mrchang@outlook.com>
2024-10-25 11:05:31 +0100

    Cleaned up webapplicationfactory. Added moq for logger to assert player management correctly processed the registration when a registration event is published

--- src/IntegrationTests/ApplicationFactoryFixture.cs
+++ src/IntegrationTests/ApplicationFactoryFixture.cs
@@ -1,15 +1,16 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Moq;
+using ILogger = Serilog.ILogger;
 
 namespace IntegrationTests;
 
 public class ApplicationFactoryFixture<TApplication> : WebApplicationFactory<TApplication> where TApplication : class
 {
     public string HostUrl { get; set; } = string.Empty;
+    public Mock<ILogger> MockLogger { get; } = new();
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -19,20 +20,16 @@ public class ApplicationFactoryFixture<TApplication> : WebApplicationFactory<TAp
     protected override IHost CreateHost(IHostBuilder builder)
     {
         var dummyHost = builder.Build();
+
+        var host = builder
+            .ConfigureWebHost(webHostBuilder => webHostBuilder.UseKestrel())
+            .ConfigureServices(services => services.AddSingleton(MockLogger.Object))
+            .Build();
         
-        builder.ConfigureWebHost(webHostBuilder => webHostBuilder.UseKestrel());
-        var host = builder.Build();
         host.Start();
 
-        var server = host.Services.GetRequiredService<IServer>();
-        var addresses = server.Features.Get<IServerAddressesFeature>();
-        
-        ArgumentNullException.ThrowIfNull(addresses);
-        ClientOptions.BaseAddress = addresses.Addresses  
-            .Select(x => new Uri(x))  
-            .Last();  
-        
         dummyHost.Start();
+        
         return dummyHost;
     }
 }
--- src/IntegrationTests/Endpoints/PlayerManagementTests.cs
+++ src/IntegrationTests/Endpoints/PlayerManagementTests.cs
@@ -1,34 +1,32 @@
 using System.Net.Http.Json;
+using Moq;
 using WebHookServerApp = WebHookServer.Program;
 using PlayerServicesApp = PlayerServices.Program;
 
 namespace IntegrationTests.Endpoints;
 
-public class PlayerManagementTests : IClassFixture<ApplicationFactoryFixture<WebHookServerApp>>, IClassFixture<ApplicationFactoryFixture<PlayerServicesApp>>
+public class PlayerManagementTests(
+    ApplicationFactoryFixture<WebHookServerApp> webHookServiceFactory,
+    ApplicationFactoryFixture<PlayerServicesApp> playerServiceFactory)
+    : IClassFixture<ApplicationFactoryFixture<WebHookServerApp>>,
+        IClassFixture<ApplicationFactoryFixture<PlayerServicesApp>>
 {
-    private readonly ApplicationFactoryFixture<WebHookServerApp> _webHookServiceFactory;
-    private readonly ApplicationFactoryFixture<PlayerServicesApp> _playerServiceFactory;
-
-    public PlayerManagementTests(ApplicationFactoryFixture<WebHookServerApp> webHookServiceFactory, ApplicationFactoryFixture<PlayerServicesApp> playerServiceFactory)
-    {
-        _webHookServiceFactory = webHookServiceFactory;
-        _playerServiceFactory = playerServiceFactory;
-    }
-
     [Fact]
     public async Task Verify_a_Valid_Player_Registration_Event_Is_Processed()
     {
         // Arrange
-        _webHookServiceFactory.HostUrl = "http://localhost:5076";
-        _playerServiceFactory.HostUrl = "http://localhost:5054";
+        webHookServiceFactory.HostUrl = "http://localhost:5076";
+        playerServiceFactory.HostUrl = "http://localhost:5054";
         
-        var webHookServiceClient = _webHookServiceFactory.CreateClient();
-        _playerServiceFactory.CreateClient();
+        var webHookServiceClient = webHookServiceFactory.CreateClient();
+        playerServiceFactory.CreateClient();
         
         // Act
-        var response = await webHookServiceClient.PostAsJsonAsync("/api/topic/publish", new { topic = "event.playerRegistration", message = new { user = "Andrew", userType = "Slot Machine"} });
-        response.EnsureSuccessStatusCode();
+        var response = await webHookServiceClient.PostAsJsonAsync("/api/topic/publish", 
+            new { topic = "event.playerRegistration", message = new { user = "Andrew", userType = "Slot Machine"} });
         
         // Assert
+        response.EnsureSuccessStatusCode();
+        playerServiceFactory.MockLogger.Verify(x => x.Information("User Registered: {User}", "Andrew"), Times.Once);
     }
 };
